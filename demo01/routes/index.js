286622dfd18d2ade8d33293c78861ff3af62b05f
kifs4 <kifs444@gmail.com>
2020-02-27 18:30:43 +0200

    27.02.2020
    Робота з node.js

---
+++ demo01/routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+    // res.render('index', { title: 'Express' });
+    res.send('Hello ' + req.query.name)
+});
+
+router.get('/make-short-name', function(req, res) {
+    // res.render('index', { title: 'Express' });
+    var s = req.query.firstname[0] + ". ";
+    var q = req.query.lastname[0] + ". ";
+    res.send('Hello ' + s + req.query.lastname)
+});
+
+module.exports = router;
